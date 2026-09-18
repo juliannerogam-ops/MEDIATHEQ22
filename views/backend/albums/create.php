16425fc1e0ff47b3e4e8bf55632c84150e7b87c0
viktoriiarudenko-source <viktoriia.rudenko@mmibordeaux.com>
2026-09-18 15:47:23 +0200

    vika

--- views/backend/albums/create.php
+++ views/backend/albums/create.php
@@ -1,3 +1,33 @@
 <?php
 include '../../../header.php';
 
+// Sécurité admin
+require_once $_SERVER['DOCUMENT_ROOT'] . '/functions/admin_guard.php';
+
+requireAdmin('page');
+
+// Charger les thématiques
+$thematiques = sql_select("THEMATIQUE", "*");
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Liste des Thématiques</h1>
+        </div>
+
+        <div class="col-md-12">
+            <form action="<?php echo ROOT_URL . '/api/thematiques/create.php'; ?>" method="post">
+
+                <div class="form-group">
+                    <label>Thématique</label>
+                    <input name="libThem" class="form-control" required>
+                </div>
+
+                <br>
+                <a href="list.php" class="btn btn-primary">List</a>
+                <button type="submit" class="btn btn-success">Confirmer create ?</button>
+            </form>
+        </div>
+    </div>
+</div>
